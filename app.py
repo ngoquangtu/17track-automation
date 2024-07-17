eca042cab5ff040e02dc577ff1c28e1544bc6591
ngoquangtu <ngoquangtuhd@gmail.com>
2024-07-17 13:33:23 +0700

    update

--- app.py
+++ app.py
@@ -142,4 +142,4 @@ def download_results(file_id):
     return send_file(output_file_path, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+     app.run(host='0.0.0.0', port=5000, debug=True)
