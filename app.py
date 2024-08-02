8cd01772ce6c60ebb3c45c59debc9e90f46af12b
ngoquangtu <ngoquangtuhd@gmail.com>
2024-08-02 15:10:39 +0700

    update

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, jsonify, render_template, request, send_file
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -13,7 +12,7 @@ import threading
 import uuid
 import os
 import time
-import sys
+
 app = Flask(__name__)
 lock = threading.Lock()
 
@@ -42,12 +41,14 @@ def process_tracking_numbers(tracking_numbers, all_data, results):
         search_box.send_keys(tracking_numbers_str)
         track_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, '.batch_track_search-area__9BaOs')))
         track_button.click()
+        
         try:
             WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CLASS_NAME, 'captcha-container')))
             results.append({"status": "error", "message": "CAPTCHA detected. Please solve it manually."})
             return
         except Exception as e:
             pass
+
         try:
             time.sleep(5)
             close_button = WebDriverWait(driver, 2).until(
@@ -123,7 +124,6 @@ def track_shipments():
     else:
         return jsonify({'status': 'error', 'message': 'File ID not provided'})
 
-    # Phân chia danh sách tracking numbers thành các phần nhỏ hơn
     chunks = [tracking_numbers[i:i+40] for i in range(0, len(tracking_numbers), 40)]
     threads = []
     all_data = []
@@ -139,19 +139,35 @@ def track_shipments():
 
     if file_path:
         try:
-            all_data_df = pd.DataFrame(all_data, columns=['Tracking',' Status', 'In transit at','Final status at',  'Time to final status at' ])
+            all_data_df = pd.DataFrame(all_data, columns=['Tracking', 'Status', 'In transit at', 'Final status at', 'Time to final status at'])
             all_data_df['In transit at'] = pd.to_datetime(all_data_df['In transit at'], format='%Y-%m-%d %H:%M', errors='coerce')
+            all_data_df['Final status at'] = pd.to_datetime(all_data_df['Final status at'], format='%Y-%m-%d %H:%M', errors='coerce')
             df['Processed at'] = pd.to_datetime(df['Processed at'], format='%Y-%m-%d %H:%M', errors='coerce')
-            all_data_df['Time in transit at'] = (all_data_df['In transit at'] - df['Processed at']).dt.total_seconds() / 3600
-            all_data_df['Refund'] = all_data_df['Time in transit at'] > 48
 
-            updated_df = pd.merge(df, all_data_df, on='Tracking', how='left')
-            updated_df.to_excel(file_path, index=False)
+            merged_df = pd.merge(all_data_df, df[['Tracking', 'Processed at']], on='Tracking', how='left')
+            merged_df['Time in transit at'] = (merged_df['In transit at'] - merged_df['Processed at']).dt.total_seconds() / 3600 +12
+            merged_df['Refund'] = merged_df['Time in transit at'] > 48
+
+            df['In transit at'] = pd.to_datetime(df['In transit at'], errors='coerce')
+            df['Final status at'] = pd.to_datetime(df['Final status at'], errors='coerce')
+
+            for index, row in merged_df.iterrows():
+                tracking_number = row['Tracking']
+                if tracking_number in df['Tracking'].values:
+                    df.loc[df['Tracking'] == tracking_number, 'Status'] = row['Status']
+                    df.loc[df['Tracking'] == tracking_number, 'In transit at'] = row['In transit at']
+                    df.loc[df['Tracking'] == tracking_number, 'Final status at'] = row['Final status at']
+                    df.loc[df['Tracking'] == tracking_number, 'Time to final status at'] = row['Time to final status at']
+                    df.loc[df['Tracking'] == tracking_number, 'Time in transit at'] = row['Time in transit at']
+                    df.loc[df['Tracking'] == tracking_number, 'Refund'] = row['Refund']
+
+            df.to_excel(file_path, index=False)
             print("Tracking information has been saved to file")
         except Exception as e:
             return jsonify({'status': 'error', 'message': str(e)})
 
     return jsonify({"status": "success"})
+
 
 @app.route('/download/<file_id>')
 def download_results(file_id):
